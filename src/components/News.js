9769603d3b8c665540d5a97786b5ec4f4a62abe9
Harshit Kumar <44989568+hksoriginal@users.noreply.github.com>
2022-01-07 04:23:05 +0530

    Coverted to function based components

--- src/components/News.js
+++ src/components/News.js
@@ -1,88 +1,90 @@
-import React, { Component, } from 'react'
+import React, { useEffect, useState } from 'react'
 import NewItems from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
 
-export class News extends Component {
+const News = (props) => {
+    const [articles, setArticles] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [page, setPage] = useState(1)
+    const [totalResults, settotalResults] = useState(0)
 
-    static defaultProps = {
-        country: 'in',
-        pageSize: 8,
-        category: 'general'
-
-    }
-    static propTypes = {
-        country: PropTypes.string,
-        pageSize: PropTypes.number,
-        category: PropTypes.string
-    }
-
-     capitalizeFirstLetter =(string)=> {
+    const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
-      }
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            articles: [],
-            loading: false,
-            page: 1
-        }
-        document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsKitty`
     }
 
 
-    async updateNews() {
-        this.props.setProgress(10);
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-        this.setState({ loading: true })
+
+
+    const updateNews = async () => {
+        props.setProgress(10);
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+        setLoading(true)
         let data = await fetch(url);
-        this.props.setProgress(30);
+        props.setProgress(30);
         let parsedData = await data.json();
-        this.props.setProgress(70);
+        props.setProgress(70);
         // console.log(parsedData);
-        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
-        this.props.setProgress(100);
+        setArticles(parsedData.articles);
+        settotalResults(parsedData.totalResults);
+        setLoading(false);
+        props.setProgress(100);
 
     }
 
-    async componentDidMount() {
-        this.updateNews();
+    useEffect(() => {
+        document.title = `${capitalizeFirstLetter(props.category)} - NewsKitty`;
+        updateNews();
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
+    const handleprev = async () => {
+        setPage(page - 1)
+        updateNews()
+    }
+    const handlenxt = async () => {
+        setPage(page + 1)
+        updateNews()
     }
 
-    handleprev = async () => {
-        this.setState({ page: this.state.page - 1 });
-        this.updateNews()
-    }
-    handlenxt = async () => {
-        this.setState({ page: this.state.page + 1 });
-        this.updateNews()
-    }
 
 
 
-    render() {
-        console.log("Render")
-        return (
-            <div className="container my-3">
-                <h1 className="text-center" style={{ margin: '40px 0px' }}>NewsKitty {this.capitalizeFirstLetter(this.props.category)} - Top Headlines</h1>
-                {this.state.loading && <Spinner />}
 
-                <div className="row">
-                    {this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <NewItems title={element.title ? element.title : ""} description={element.description ? element.description : ""} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
-                        </div>
+    return (
+        <div className="container my-3">
+            <h1 className="text-center" style={{ margin: '40px 0px', marginTop: '60px'}}>NewsKitty {capitalizeFirstLetter(props.category)} - Top Headlines</h1>
+            {loading && <Spinner />}
 
-                    })}
-                    <div className="container d-flex justify-content-between">
-                        <button disabled={this.state.page <= 1} type="button" onClick={this.handleprev} className="btn btn-dark">&larr; Previous</button>
-                        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" onClick={this.handlenxt} className="btn btn-dark">Next &rarr;</button>
+            <div className="row">
+                {articles.map((element) => {
+                    return <div className="col-md-4" key={element.url}>
+                        <NewItems title={element.title ? element.title : ""} description={element.description ? element.description : ""} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
                     </div>
+
+                })}
+                <div className="container d-flex justify-content-between">
+                    <button disabled={page <= 1} type="button" onClick={handleprev} className="btn btn-dark">&larr; Previous</button>
+                    <button disabled={page + 1 > Math.ceil(totalResults / props.pageSize)} type="button" onClick={handlenxt} className="btn btn-dark">Next &rarr;</button>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+
+}
+
+News.defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general'
+
+}
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
 }
 
 export default News
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export class NewItems extends Component {
-    render() {
-        let {title,description,imgUrl,newsUrl,date,author} = this.props;
+const NewItems  = (props)=> {
+    
+        let {title,description,imgUrl,newsUrl,date,author} = props;
         return (
             <div className="my-3">
                 <div className="card" >
@@ -16,7 +16,7 @@ export class NewItems extends Component {
                 </div>
             </div>
         )
-    }
+    
 }
 
 export default NewItems
